80e519703e7dd0db4cb016ad6cc07cf33a529e2b
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-02-01 00:18:35 +0000

    Styling

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <img src={this.state.imageUrl} alt="" />
+        <span className="badge badge-primary m-2">{this.state.count}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
