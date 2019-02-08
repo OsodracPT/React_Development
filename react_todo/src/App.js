b76f0622da5569ce8f54cbbd01b5670dc6d861a0
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-02-08 22:21:50 +0000

    PropTypes

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take the trash out",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Meeting with manager",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Grocerie shopping",
+        completed: false
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
