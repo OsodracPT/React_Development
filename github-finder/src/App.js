955efad05b5c27759fd1f38b835a21296ae6347b
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-08-24 11:46:48 +0100

    React Router

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
+import About from "./components/pages/About";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
@@ -47,22 +49,38 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar title="Github Finder" icon="fab fa-github" />
+      <Router>
+        <div className="App">
+          <Navbar title="Github Finder" icon="fab fa-github" />
 
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            setAlert={this.setAlert}
-            showClear={this.state.users.length > 0 ? true : false}
-          >
-            {" "}
-          </Search>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      setAlert={this.setAlert}
+                      showClear={this.state.users.length > 0 ? true : false}
+                    >
+                      {" "}
+                    </Search>
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
