7c44e8c1805169f07aa314ebdd34bad36aba7146
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-08-24 20:04:40 +0100

    Creating reducer and actions

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -18,20 +18,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search GitHub users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   //Get a single user from github
   const getUser = async username => {
     setLoading(true);
@@ -84,7 +70,6 @@ const App = () => {
                 render={props => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       setAlert={showAlert}
                       showClear={users.length > 0 ? true : false}
