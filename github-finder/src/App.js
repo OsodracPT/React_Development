2592ff28a2552788966cf7131cf0ac1cf501670c
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-08-25 03:46:15 +0100

    Home & NotFound Components

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import About from "./components/pages/About";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -21,18 +21,10 @@ const App = () => {
             <div className="container">
               <Alert alert={alert} />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={props => (
-                    <Fragment>
-                      <Search> </Search>
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
