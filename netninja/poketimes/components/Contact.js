9405308acc3a0d6f37ef20124532b091924d77f6
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-02-20 22:54:01 +0000

    poketimes create-app

---
+++ netninja/poketimes/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>Lorem ipsum</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ netninja/poketimes/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Poketimes</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
